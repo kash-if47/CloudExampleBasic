e4928578bf4dc40c4cc085eed87c804e3479aa58
kash-if47 <kashif.iqbal@mavs.uta.edu>
2019-06-05 08:42:21 -0500

    Part3 Done

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 """Cloud Foundry test"""
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
 import os
 
 app = Flask(__name__)
@@ -7,9 +8,21 @@ app = Flask(__name__)
 print(os.getenv("PORT"))
 port = int(os.getenv("PORT", 5000))
 
+conn = sqlite3.connect('myDb.db')
+print("Opened database successfully")
+cur = conn.cursor()
+cur.execute('select * from student')
+rows = cur.fetchall()
+
 @app.route('/')
-def hello_world():
-    return 'Hello World! I am running on port ' + str(port)
+def home():
+   return render_template('home.html')
+
+@app.route('/list')
+def list():
+    # return 'Hello World! I am running on port ' + str(port)
+    # d = data()
+    return render_template("list.html",rows = rows)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
