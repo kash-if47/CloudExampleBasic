8201595fd2d24012b9d84060db4b76062bcc2d61
kash-if47 <kashif.iqbal@mavs.uta.edu>
2019-06-07 20:43:13 -0500

    Completed Part4

--- hello.py
+++ hello.py
@@ -8,21 +8,63 @@ app = Flask(__name__)
 print(os.getenv("PORT"))
 port = int(os.getenv("PORT", 5000))
 
-conn = sqlite3.connect('myDb.db')
-print("Opened database successfully")
-cur = conn.cursor()
-cur.execute('select * from student')
-rows = cur.fetchall()
+
 
 @app.route('/')
 def home():
    return render_template('home.html')
 
+@app.route('/student')
+def student():
+   return render_template('student.html')
+
+@app.route('/delstudentpage')
+def delstudentpage():
+   return render_template('delstudentpage.html')
+
 @app.route('/list')
 def list():
-    # return 'Hello World! I am running on port ' + str(port)
-    # d = data()
+    conn = sqlite3.connect('myDb.db')
+    cur = conn.cursor()
+    cur.execute('select * from student')
+    rows = cur.fetchall()
+    conn.close()
     return render_template("list.html",rows = rows)
+
+@app.route('/delstudent',methods = ['POST', 'GET'])
+def delstudent():
+    id1 = request.form['id']
+    con = sqlite3.connect('myDb.db')
+    cur = con.cursor()
+    cur.execute("DELETE from student where id = ?", (id1,))
+    con.commit()
+    cur.execute('select * from student')
+    rows = cur.fetchall()
+    con.close()
+    return render_template("list.html", rows = rows)
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+    id1 = request.form['id']
+    ln = request.form['ln']
+    fn = request.form['fn']
+    add = request.form['add']
+    cty = request.form['cty']
+    eyr = request.form['eyr']
+    nts = request.form['nts']
+    con = sqlite3.connect('myDb.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO student (id,lastname,firstname,address,city,enrollmentyear,notes) VALUES (?,?,?,?,?,?,?)",(id1,ln,fn,add,cty,eyr,nts) )
+    con.commit()
+    con.close()
+    cur = con.cursor()
+    con = sqlite3.connect('myDb.db')
+    cur.execute('select * from student')
+    rows = cur.fetchall()
+    con.close()
+    return render_template("list.html", rows = rows)
+
+         
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
